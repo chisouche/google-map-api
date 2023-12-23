adfcb2f8a65a27f4d5cb1701f703923ca41a395d
chisouche <jenniferuc.ju@gmail.com>
2023-12-23 15:32:20 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { GoogleMap, InfoWindow, Marker, useJsApiLoader } from "@react-google-maps/api";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const App = () => {
+
+    const { isLoaded, loadError } = useJsApiLoader({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY
+    });
+
+    const initialMarkers = [
+        {
+            position: {
+                lat: 28.625485,
+                lng: 79.821091
+            },
+            label: { color: "white", text: "P1" },
+            draggable: true
+        },
+        {
+            position: {
+                lat: 28.625293,
+                lng: 79.817926
+            },
+            label: { color: "white", text: "P2" },
+            draggable: false
+        },
+        {
+            position: {
+                lat: 28.625182,
+                lng: 79.81464
+            },
+            label: { color: "white", text: "P3" },
+            draggable: true
+        },
+    ];
+    
+    const [activeInfoWindow, setActiveInfoWindow] = useState(null);
+    const [markers, setMarkers] = useState(initialMarkers);
+
+    const containerStyle = {
+        width: "100%",
+        height: "400px",
+    }
+
+    const center = {
+        lat: 28.626137,
+        lng: 79.821603,
+    }
+
+    const mapClicked = (event) => { 
+        console.log(event.latLng.lat(), event.latLng.lng()) 
+    }
+
+    const markerClicked = (marker, index) => {  
+        setActiveInfoWindow(index) 
+        console.log(marker, index) 
+    }
+
+    const markerDragEnd = (event, index) => { 
+        console.log(event.latLng.lat(), event.latLng.lng())
+    }
+
+    return (
+        isLoaded
+        &&
+        <GoogleMap 
+            mapContainerStyle={containerStyle} 
+            center={center} 
+            zoom={15}
+            onClick={mapClicked}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+            {markers.map((marker, index) => (
+                <Marker 
+                    key={index} 
+                    position={marker.position}
+                    label={marker.label}
+                    draggable={marker.draggable}
+                    onDragEnd={event => markerDragEnd(event, index)}
+                    onClick={event => markerClicked(marker, index)} 
+                >
+                    {
+                        (activeInfoWindow === index)
+                        &&
+                        <InfoWindow 
+                            position={marker.position} 
+                            onCloseClick={() => setActiveInfoWindow(null)}
+                        >
+                            <b>{marker.position.lat}, {marker.position.lng}</b>
+                        </InfoWindow>
+                    }  
+                </Marker>
+            ))}
+        </GoogleMap>
+    );
+};
 
 export default App;
